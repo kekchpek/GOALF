da3095279f1414f7ceb2a28a783913616b1c5465
Николай <rfka1@inbox.ru>
2017-05-29 02:25:51 +0300

    A1

---
+++ Assets/MyAnimation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System;
+using UnityEditor;
+
+public class MyAnimation : ScriptableObject {
+    public Sprite[] frames;
+    public float playTime;
+
+
+    [MenuItem("Assets/Create/MyAnimation")]
+    public static void CreateMyAnim()
+    {
+        CreateSO.CreateAsset<MyAnimation>();
+    }
+
+
+    public Sprite GetCurrentSprite(float t)
+    {
+        return frames[Mathf.RoundToInt(t * (frames.Length-1) / playTime)];
+    }
+}
---
+++ Assets/MyAnimator.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MyAnimator : MonoBehaviour {
+    [SerializeField]
+    public List<string> animNames;
+    public List<MyAnimation> anims;
+    private Dictionary<string, MyAnimation> animations;
+    public string  currentAnimation;
+    private float currentTime;
+    private SpriteRenderer spriteRenderer;
+
+    public delegate void VoidDelegate();
+    VoidDelegate OnAnimationPlayed;
+
+	void Start () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        currentTime = 0;
+        animations = new Dictionary<string, MyAnimation>();
+        for(int i = 0; i < animNames.Count; i++)
+        {
+            animations.Add(animNames[i], anims[i]);
+        }
+	}
+	
+
+	void Update () {
+        if (currentAnimation != "")
+        {
+            if (!animations.ContainsKey(currentAnimation))
+            {
+                currentAnimation = "";
+            }
+            else
+            {
+                currentTime += Time.deltaTime;
+                if (currentTime >= animations[currentAnimation].playTime)
+                {
+                    AnimationPlayed();
+                    currentTime -= animations[currentAnimation].playTime;
+                }
+                spriteRenderer.sprite = animations[currentAnimation].GetCurrentSprite(currentTime);
+            }
+        }
+	}
+
+    void AnimationPlayed()
+    {
+        if(OnAnimationPlayed != null)
+            OnAnimationPlayed();
+    }
+
+}
